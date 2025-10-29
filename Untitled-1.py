ec18005f8667854b807beec4eb6dec5636b6edb4
Md. Touhidul Islam <110239106+touhid0503@users.noreply.github.com>
2025-10-29 11:47:50 +0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,25 @@
+
+
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+file_path = "mushrooms.csv"
+
+df = kagglehub.load_dataset(
+    KaggleDatasetAdapter.PANDAS,
+    "uciml/mushroom-classification",
+    file_path,
+)
+
+print("✅ Dataset loaded successfully!")
+print("Shape:", df.shape)
+print(df.columns.tolist())
+print(df.head())
+print(df['class'].value_counts())
+
+df['class'].map({'e':0, 'p':1})
+
+# Download latest version
+path = kagglehub.dataset_download("uciml/mushroom-classification")
+
+print("Path to dataset files:", path)
